1c924cac55ee3095d03cadb29f093809f7c74cf3
Jedrzej Malaczynski <jedrzejmalaczynski@gmail.com>
2026-01-11 23:29:27 +0100

    fixed

--- app/core/database.py
+++ app/core/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import declarative_base
+import ssl
 
 from app.core.config import settings
 
@@ -12,6 +13,15 @@ async_engine = create_async_engine(DATABASE_URL, echo=settings.DEBUG)
 async_session = async_sessionmaker(async_engine, expire_on_commit=False)
 Base = declarative_base()
 
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
+async_engine = create_async_engine(
+    settings.SQLALCHEMY_DATABASE_URI,
+    connect_args={"ssl": ssl_context},
+    echo=False
+)
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     """FastAPI dependency to get a database session."""
